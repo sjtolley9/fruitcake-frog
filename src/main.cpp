6077d08eb06557820c6f91b1d684d6fa17da1019
Sterling Tolley <sterlingj.tolley@gmail.com>
2021-12-27 19:26:48 -0700

    Animated background and line

--- src/main.cpp
+++ src/main.cpp
@@ -9,26 +9,35 @@
 
 int main(int argc, char *argv[])
 {
-  SDL_Init(SDL_INIT_VIDEO);
+	SDL_Init(SDL_INIT_VIDEO);
 
-  SDL_Window *window = SDL_CreateWindow(
-    "Fruitcake Frog",
-    SDL_WINDOWPOS_UNDEFINED,
-    SDL_WINDOWPOS_UNDEFINED,
-    640,
-    480,
-    0
-  );
+	SDL_Window *window = SDL_CreateWindow(
+			"Fruitcake Frog",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			640,
+			480,
+			0
+			);
 
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
 
-  SDL_Delay(3000);
+	for (int i = 0; i < 255; i++) {
+		SDL_SetRenderDrawColor(renderer, i, 255-i, 0, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(renderer);
 
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+		SDL_RenderDrawLine(renderer, 0, 480*i/255, 640, 480);
 
-  return 0;
+		SDL_RenderPresent(renderer);
+		SDL_Delay(1000/60);
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
 }
