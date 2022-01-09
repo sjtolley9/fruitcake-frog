83caa4f50fb7210e001834d7638887e345e4368f
Sterling Tolley <sterlingj.tolley@gmail.com>
2022-01-09 13:52:47 -0700

    Added TTF font support, not ready yet. Refactoring

---
+++ src/LTexture.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include "LTexture.hpp"
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_ttf.h"
+
+SDL_Renderer* gRenderer;
+TTF_Font *gFont = NULL;
+
+LTexture::LTexture() {
+	//Initialize
+	mTexture = NULL;
+	mWidth = 0;
+	mHeight = 0;
+}
+
+LTexture::~LTexture() {
+	//Deallocate
+	free();
+}
+
+bool LTexture::loadFromFile( std::string path) {
+	// Get rid of preexisting texture
+	free();
+
+
+	// The final texture
+	SDL_Texture* newTexture = NULL;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+	if ( loadedSurface == NULL ) {
+		std::cout << "Unable to load image" << std::endl;
+	} else {
+		// Color key image
+		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
+
+		//Create Texture
+		newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+		if ( newTexture == NULL ) {
+			std::cout << "Texture couldn't load" << std::endl;
+		} else {
+			mWidth = loadedSurface->w;
+			mHeight = loadedSurface->h;
+		}
+
+		//Free Surface
+		SDL_FreeSurface( loadedSurface );
+	}
+
+	//Return success
+	mTexture = newTexture;
+	return mTexture != NULL;
+}
+
+void LTexture::free() {
+	if (mTexture != NULL) {
+		SDL_DestroyTexture( mTexture );
+		mTexture = NULL;
+		mWidth = 0;
+		mHeight = 0;
+	}
+}
+
+void LTexture::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip) {
+	//Set rendering space and render to screen
+	SDL_Rect renderQuad = {x, y, mWidth, mHeight};
+
+	if (clip != NULL) {
+		renderQuad.w = clip->w;
+		renderQuad.h = clip->h;
+	}
+
+	SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad, angle, center, flip);
+}
+
+bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColor ) {
+	// Free any preexisting texture
+	free();
+
+	// Render text surface
+	SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, textureText.c_str(), textColor );
+	if (textSurface == NULL) {
+		std::cout << "Unable to render text surface!" << std::endl;
+	} else {
+		// Create Texture
+		mTexture = SDL_CreateTextureFromSurface(gRenderer, textSurface );
+		if ( mTexture == NULL ) {
+			std::cout << "That didn't seem to work!" << std::endl;
+		} else {
+			mWidth = textSurface->w;
+			mHeight = textSurface->h;
+		}
+
+		// Free old surface
+		SDL_FreeSurface( textSurface );
+	}
+
+	return mTexture != NULL;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,10 @@
 #include <iostream>
 #include "SDL.h" 
 #include "SDL_image.h"
+#include "SDL_ttf.h"
+#include "LTexture.hpp"
 
 SDL_Window *gWindow;
-SDL_Renderer* gRenderer;
 
 SDL_Rect gSpriteClips[ 40*31 ];
 
@@ -59,149 +60,12 @@ bool init() {
 	return success;
 }
 
-SDL_Texture* loadTexture( std::string path) {
-	SDL_Texture* newTexture = NULL;
-	
-	SDL_Surface* loadedSurface = IMG_Load(path.c_str() );
-	if ( loadedSurface == NULL ) {
-		std::cout << "Unable to load image" << std::endl;
-	} else {
-		newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
-		if (newTexture == NULL) {
-			std::cout << "Unable to create texture" << std::endl;
-		}
-		
-		SDL_FreeSurface( loadedSurface );
-	}
-
-	return newTexture;
-}
-
-// Texture Wrapper Class
-class LTexture {
-	public:
-		//Initializer
-		LTexture();
-
-		//Destructor
-		~LTexture();
-
-		//Loads image at path
-		bool loadFromFile( std::string path );
-
-		//Deallocates texture
-		void free();
-
-		//Renders texture at given point
-		void render( int x, int y, SDL_Rect* clip = NULL );
-
-		//Gets image dimensions
-		int getWidth();
-		int getHeight();
-	private:
-		//Texture
-		SDL_Texture* mTexture;
-
-		//Dimensions
-		int mWidth;
-		int mHeight;
-};
-
-LTexture::LTexture() {
-	//Initialize
-	mTexture = NULL;
-	mWidth = 0;
-	mHeight = 0;
-}
-
-LTexture::~LTexture() {
-	//Deallocate
-	free();
-}
-
-bool LTexture::loadFromFile( std::string path) {
-	// Get rid of preexisting texture
-	free();
-
-	for (int i = 0; i < 40; i++) {
-		for (int j = 0; j < 31; j++) {
-			gSpriteClips[i + j*40].x = 16*i;
-			gSpriteClips[i + j*40].y = 16*j;
-			gSpriteClips[i + j*40].w = 16;
-			gSpriteClips[i + j*40].h = 16;
-		}
-	}
-
-	// The final texture
-	SDL_Texture* newTexture = NULL;
-
-	//Load image at specified path
-	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-	if ( loadedSurface == NULL ) {
-		std::cout << "Unable to load image" << std::endl;
-	} else {
-		// Color key image
-		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
-
-		//Create Texture
-		newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
-		if ( newTexture == NULL ) {
-			std::cout << "Texture couldn't load" << std::endl;
-		} else {
-			mWidth = loadedSurface->w;
-			mHeight = loadedSurface->h;
-		}
-
-		//Free Surface
-		SDL_FreeSurface( loadedSurface );
-	}
-
-	//Return success
-	mTexture = newTexture;
-	return mTexture != NULL;
-}
-
-void LTexture::free() {
-	if (mTexture != NULL) {
-		SDL_DestroyTexture( mTexture );
-		mTexture = NULL;
-		mWidth = 0;
-		mHeight = 0;
-	}
-}
-
-void LTexture::render( int x, int y, SDL_Rect* clip) {
-	//Set rendering space and render to screen
-	SDL_Rect renderQuad = {x, y, mWidth, mHeight};
-
-	if (clip != NULL) {
-		renderQuad.w = clip->w;
-		renderQuad.h = clip->h;
-	}
-
-	SDL_RenderCopy( gRenderer, mTexture, clip, &renderQuad);
-}
-
 int main(int argc, char *argv[])
 {
 	init();
 
-	LTexture texture1, texture2;
-	texture1.loadFromFile("assets/CL_DEMO.png");
-	texture2.loadFromFile("assets/higo.png");
-
-	/*SDL_Init(SDL_INIT_VIDEO);
-
-	SDL_Window *window = SDL_CreateWindow(
-			"Fruitcake Frog",
-			SDL_WINDOWPOS_UNDEFINED,
-			SDL_WINDOWPOS_UNDEFINED,
-			640,
-			480,
-			0
-			);*/
-
-	
+	LTexture splashTexture;
+	splashTexture.loadFromFile("assets/splash_logo_small.png");
 
 	Uint32 elapsedTime = 0, currentTime = SDL_GetTicks(), oldTime = SDL_GetTicks();
 
@@ -228,13 +92,7 @@ int main(int argc, char *argv[])
 		if (elapsedTime == 0) {
 			SDL_RenderClear( gRenderer );
 
-			for (int i = 0; i < 40; i++) {
-				for (int j = 0; j < 31; j++) {
-					texture1.render(17*i,17*j,&gSpriteClips[i+j*40]);
-					//texture1.render(16*i,16*j,&gSpriteClips[13]);
-				}
-			}
-			texture2.render(10,10);
+			splashTexture.render(0,0);
 
 			SDL_RenderPresent( gRenderer );
 		}
